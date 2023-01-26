3deedfa0438aefa24492005aae537b09d1a4f3c8
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-01-27 00:14:33 +0530

    Update index.js

--- Arrays - Map, Filter, Reduce/index.js
+++ Arrays - Map, Filter, Reduce/index.js
@@ -1,5 +1,6 @@
 /// Map ///
-let arr = [1, 8, 9, 4];
+const arr = [1, 8, 9, 4];
+
 // Map - Inbuilt
 const newArr = arr.map((element, elementIndex, array) => {
     return element * 2;
@@ -20,6 +21,32 @@ const newArr = arr.myMap((element, elementIndex, array) => {
 })
 
 //output = [ 2, 16, 18, 8 ]
+
+
+/// Filter ///
+const arr = [1, 8, 9, 4];
+
+// Fitler - Inbuilt
+const newArr = arr.filter((element, elementIndex, array) => {
+    return element > 2;
+})
+
+// Filter - Polyfill
+Array.prototype.myFilter = function (cbFunction) {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (cbFunction(this[i], i, this)) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+}
+
+const newArr = arr.myFilter((element, elementIndex, array) => {
+    return element > 2;
+})
+
+// output = [ 8, 9, 4] 
 
 
 
