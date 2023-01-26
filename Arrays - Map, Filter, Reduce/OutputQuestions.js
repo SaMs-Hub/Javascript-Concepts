8158f5fd9dc08dd9eb802643fd1d7a965ccb5102
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-01-27 00:58:29 +0530

    Update OutputQuestions.js

--- Arrays - Map, Filter, Reduce/OutputQuestions.js
+++ Arrays - Map, Filter, Reduce/OutputQuestions.js
@@ -25,4 +25,28 @@ console.log(filteredStudents);
 // Output - [ { name: 'Sam', rollNumber: 17, marks: 80 } ]
 
 
+// Q4 - Return total marks of all students
+const totalMarks = students.reduce((acc, curr) => {
+    return acc + curr.marks;
+}, 0)
+console.log(totalMarks);
+// Output - 239
+
+
+// Q5 - Return student names with more than 60 marks
+const filteredStudents = students.filter((student) => {
+    return student.marks > 60
+}).map((student) => {
+    return student.name
+})
+console.log(filteredStudents);
+// Output - [ 'Sam', 'Mohit' ]
+
+
+
+
+
+
+
+
 
