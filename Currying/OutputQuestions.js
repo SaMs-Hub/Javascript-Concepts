7e9d66cc7cfb000f4e511d36d38ccbbbbb35f5a9
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-01-29 00:40:07 +0530

    Update OutputQuestions.js

--- Currying/OutputQuestions.js
+++ Currying/OutputQuestions.js
@@ -9,3 +9,28 @@ const curriedExample = (a) => {
 console.log(curriedExample(3)(4)); // 3 4
 
 
+// Q2. Implement sum(a)(b)(c)
+Ans:
+const sum = (a) => {
+    return (b) => {
+        return (c) => {
+            return a + b + c;
+        }
+    }
+}
+
+console.log(sum(1)(3)(4)); // 8
+
+// Q3. Infinite currying sum(a)(b)..()
+Ans:
+const add = (a) => {
+    return (b) => {
+        if (b) return add(a + b);
+        return a;
+    }
+}
+
+console.log(add(1)(3)(4)());
+
+
+
