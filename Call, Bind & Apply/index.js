1d32837756ad1b22090711d489574d54cfe90538
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-01-31 00:06:16 +0530

    Update index.js

--- Call, Bind & Apply/index.js
+++ Call, Bind & Apply/index.js
@@ -10,18 +10,34 @@ function sayHello(age) {
 console.log(sayHello.call(obj, 33)); // hello sam whose age is 33
 
 
-
-
 // Call Polyfill
+let obj = {
+    name: 'sam',
+}
+function sayHello(age) {
+    console.log("hello " + this.name + " whose age is " + age);
+}
+Function.prototype.customCall = function (context, ...args) {
+    if (typeof this !== "function") throw new Error("not a function");
+    context.fnRef = this;
+    context.fnRef(...args);
 
-
+}
 
 // Apply
 console.log(sayHello.apply(obj, [33])); // hello sam whose age is 33
 
 
-
 // Apply Polyfill
+Function.prototype.customApply = function (context = {}, args = []) {
+    if (typeof this !== "function") throw new Error("not a function");
+    if (!Array.isArray(args)) throw new Error("Provide array");
+    context.fnRef = this;
+    context.fnRef(...args);
+
+}
+
+sayHello.customApply(obj, [33]); 
 
 
 // Bind
@@ -33,3 +49,15 @@ console.log(binded(44)); // hello sam whose age is 44
 
 
 // Bind Polyfill
+Function.prototype.customBind = function (context = {}, ...args) {
+    if (typeof this !== "function") throw new Error("not a function");
+
+    context.fnRef = this;
+    return function (...newArgs) {
+        return context.fnRef(...args, ...newArgs);
+    }
+
+}
+
+const bindFunc = sayHello.customBind(obj);
+bindFunc(33);
