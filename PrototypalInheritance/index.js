e6eca31870b8834d067693f79f95ec942a161c31
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-02-03 00:20:38 +0530

    Create index.js

---
+++ PrototypalInheritance/index.js
@@ -0,0 +1,21 @@
+
+let obj = {
+    name: 'sam',
+    city: 'hyderabad',
+    getName: function(){
+        return `${this.name} lives in ${this.city}`
+    }
+};
+
+
+let obj2 = {
+    name: 'mes',
+}
+
+obj2.__proto__ = obj;
+
+console.log(obj.name); // sam
+console.log(obj2.name); // mes
+console.log(obj2.city); // hyderabad
+console.log(obj.getName()); // sam lives in hyderabad
+console.log(obj2.getName()); // mes lives in hyderabad
