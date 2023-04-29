2646348e7984c45cf5ea7b9dc083b78d0b8506c0
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-04-30 00:26:00 +0530

    Update Reduce.js

--- Arrays - Map, Filter, Reduce/Reduce.js
+++ Arrays - Map, Filter, Reduce/Reduce.js
@@ -38,4 +38,50 @@ const segregate = arr.reduce((pv, cv) => {
 console.log(segregate);
 
 
-// 
+// Can also be used to run things in sequence
+const upperCase = (str) => {
+    return str.toUpperCase();
+}
+
+const reverse = (str) => {
+    return str.split('').reverse().join('');
+}
+
+const append = (str) => {
+    return "Hello + " + str;
+}
+
+const arr = [append, reverse, upperCase];
+
+const fV = arr.reduce((pv, cv) => {
+    const newValue = cv(pv);
+    console.log(`${cv}, ${newValue}`)
+    return newValue
+}, "sam");
+
+
+
+// same with promises
+const asyncTask = (time) => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(`completed in ${time}`)
+        }, 100 * time);
+    })
+}
+
+const promises = [
+    asyncTask(7),
+    asyncTask(3),
+    asyncTask(1),
+]
+
+const resPromises = (promises) => {
+    promises.reduce((acc, curr) => {
+        return acc.then(() => {
+            return curr.then((val) => console.log(val))
+        })
+    }, Promise.resolve())
+}
+
+resPromises(promises);
