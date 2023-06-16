cbe84fa02b7ef03c6fc7d783fdb7340fe6b0f929
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2023-06-16 14:37:04 +0530

    Update and rename Promises/Polyfills.js to Promises/Polyfills/1. PromiseAll.js

--- Promises/Polyfills/1. PromiseAll.js
+++ Promises/Polyfills/1. PromiseAll.js
@@ -1,4 +1,4 @@
-// Promise all polyfill
+// Promise all polyfill - 1st Approach
 const step1 = (v) => {
     return new Promise((res, rej) => {
         setTimeout(() => {
@@ -59,4 +59,41 @@ Promise.customAll([
 }).catch((err) => {
     console.log(err);
 })
+
+
+// Approach 2
+const doThis = (time) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (time < 3000) rej("rejected");
+      else res(time);
+    });
+  });
+};
+
+const helloWorld = () => {
+  return Promise.resolve("hello world");
+};
+
+const customPromiseAll = (taskArr) => {
+  let results = [];
+  let completedPromises = 0;
+  return new Promise((res, rej) => {
+    taskArr.forEach((pro, index) => {
+      pro
+        .then((val) => {
+          results[index] = val;
+          completedPromises += 1;
+
+          if (completedPromises == taskArr.length) res(results);
+        })
+        .catch((err) => rej(err));
+    });
+  });
+};
+
+customPromiseAll([doThis(3000), doThis(4000), doThis(3000), helloWorld()])
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
 
